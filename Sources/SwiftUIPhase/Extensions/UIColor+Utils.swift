9aee76205cd62cad086827241e25bb55ed64b5b9
Jeremy Vineyard <coinbump@gmail.com>
2020-08-23 10:26:32 -0700

    Add another color utility

--- Sources/SwiftUIPhase/Extensions/UIColor+Utils.swift
+++ Sources/SwiftUIPhase/Extensions/UIColor+Utils.swift
@@ -21,4 +21,17 @@ extension UIColor {
 
         return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
     }
+
+    public func saturatedBy(_ amount: CGFloat) -> UIColor {
+        var hue: CGFloat = 0.0
+        var saturation: CGFloat = 0.0
+        var brightness: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+
+        self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha)
+
+        saturation = min(1.0, max(0, saturation + amount))
+
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+    }
 }
---
+++ Sources/SwiftUIPhase/Styles/StyleContextType.swift
@@ -0,0 +1,15 @@
+//
+//  StyleContextType.swift
+//  
+//
+//  Created by Jeremy Vineyard on 8/23/20.
+//
+
+import Foundation
+
+/// Defines a context for a style. Example: a style looks different when interactive vs non interactive
+enum StyleContextType: String {
+    // Display-only content
+    case display
+    case interactive
+}
