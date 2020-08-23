719e4082e2f1e554c5f70cb05f95658bf99feaa6
Jeremy Vineyard <coinbump@gmail.com>
2020-08-23 11:17:59 -0700

    Update ProgressBar.swift

--- Sources/SwiftUIPhase/Views/ProgressBar.swift
+++ Sources/SwiftUIPhase/Views/ProgressBar.swift
@@ -8,31 +8,38 @@
 import Foundation
 import SwiftUI
 
+private enum Constants {
+    static let progressBarHeight: CGFloat = 4.0
+}
+
 /// ProgressView is only available in iOS 14, this gives us one for iOS 13
 // FUTURE: support label
-public struct ProgressBar: View {
+public struct ProgressBar<Label>: View where Label: View {
     let value: Double
     let total: Double
 
-    private enum Constants {
-        static let progressBarHeight: CGFloat = 4.0
+    public init(value: Double, total: Double = 1.0) where Label == EmptyView {
+        self.value = value
+        self.total = total
     }
 
     public var body: some View {
         if #available(iOS 14.0, *) {
             ProgressView(value: value, total: total)
         }
-        ZStack {
-            GeometryReader { geometry in
-                RoundedRectangle(cornerRadius: Constants.progressBarHeight/2.0)
-                    .fill(Color(.systemGray5))
-                RoundedRectangle(cornerRadius: Constants.progressBarHeight/2.0)
-                    .fill(Color(.systemBlue))
-                    .frame(width: geometry.size.width*CGFloat(value), height: nil, alignment: .center)
+        else {
+            ZStack {
+                GeometryReader { geometry in
+                    RoundedRectangle(cornerRadius: Constants.progressBarHeight/2.0)
+                        .fill(Color(.systemGray5))
+                    RoundedRectangle(cornerRadius: Constants.progressBarHeight/2.0)
+                        .fill(Color(.systemBlue))
+                        .frame(width: geometry.size.width*CGFloat(value), height: nil, alignment: .center)
+                }
             }
+            .drawingGroup()
+            .frame(minWidth: nil, idealWidth: nil, maxWidth: .infinity, minHeight: Constants.progressBarHeight, idealHeight: Constants.progressBarHeight, maxHeight: Constants.progressBarHeight, alignment: .center)
         }
-        .drawingGroup()
-        .frame(minWidth: nil, idealWidth: nil, maxWidth: .infinity, minHeight: Constants.progressBarHeight, idealHeight: Constants.progressBarHeight, maxHeight: Constants.progressBarHeight, alignment: .center)
     }
 }
 
