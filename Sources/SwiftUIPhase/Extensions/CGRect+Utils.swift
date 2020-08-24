511ee1fad7454b5e24f8cd46c51183659d63fb86
Jeremy Vineyard <coinbump@gmail.com>
2020-08-23 23:02:14 -0700

    HueRing

---
+++ Sources/SwiftUIPhase/Extensions/CGRect+Utils.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  SwiftUIPhase
+//
+//  Created by Jeremy Vineyard on 8/23/20.
+//
+
+import Foundation
+import CoreGraphics
+
+extension CGRect {
+    public var center: CGPoint { CGPoint(x: midX, y: midY) }
+}
